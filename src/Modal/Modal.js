19701324683cff299d1ff91e47cddfa292e2c7fa
Alexander Lyakhnitskiy <qistersick@gmail.com>
2020-05-22 19:32:13 +0300

    fake login improved

--- src/Modal/Modal.js
+++ src/Modal/Modal.js
@@ -3,25 +3,54 @@ import './Modal.css'
 import Context from '../context'
 
 export default function Modal() {
-    const [isOpen, setIsOpen] = useState(false)
+    const [isOpen, setIsOpen] = useState(true)
 
     const {setLogin} = useContext(Context)
-    const login = (name) => {
+    
+    const login = (e, name) => {
+        e.preventDefault()
         setLogin(name)
         setIsOpen(false)
     }
 
     return (
-        <React.Fragment>
-            <button onClick={() => setIsOpen(true)}>log in</button>
-            {isOpen && (
-                <div className='modal'>
-                    <div className='modal-body'>
-                        <p>Login as:</p>
-                        <button onClick={() => login('Jonh')}>Jonh</button>
-                        <button onClick={() => login('Lily')}>Lily</button>
-                    </div>
-                </div>)}
-        </React.Fragment>
+    <React.Fragment>
+    <button onClick={() => setIsOpen(true)}>log in</button>
+    {isOpen && (
+        <div className='modal'>
+            <div className='modal-body'>
+                <form onSubmit={(e) => login(e, document.getElementById('loginField').value)}>
+                    <table>
+                        <tbody>
+                            <tr>
+                                <td><label >Логин</label></td>
+                                <td><input id="loginField" type="text" name="login"/></td>
+                            </tr>
+                            <tr>
+                                <td><label >Пароль</label></td>
+                                <td><input id="passwordField" type="password" name="password"/></td>
+                            </tr>
+                            <tr>
+                                <td ><input type="submit" value="Войти"/></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </form>
+            </div>
+        </div>
+    )}
+    </React.Fragment>
     )
 }
+
+        // <React.Fragment>
+        //     <button onClick={() => setIsOpen(true)}>log in</button>
+        //     {isOpen && (
+        //         <div className='modal'>
+        //             <div className='modal-body'>
+        //                 <p>Login as:</p>
+        //                 <button onClick={() => login('Jonh')}>Jonh</button>
+        //                 <button onClick={() => login('Lily')}>Lily</button>
+        //             </div>
+        //         </div>)}
+        // </React.Fragment>
